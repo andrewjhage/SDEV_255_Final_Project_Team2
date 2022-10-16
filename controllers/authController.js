6fbec33614d2542baf4c0c50a6241b1af23a2738
Iri-Z <94073424+Iri-Z@users.noreply.github.com>
2022-10-16 19:28:31 +0000

    Footer change

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const { Student, Teacher }= require('../models/User');
+const { Student, Teacher } = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 // handle errors
@@ -45,27 +45,27 @@ const createUserToken = (id) => {
   });
 };
 const createTeacherToken = (id) => {
-    return jwt.sign({ id }, 'sdev 255 final teacher', {
-      expiresIn: maxAge
-    });
-  };
+  return jwt.sign({ id }, 'sdev 255 final teacher', {
+    expiresIn: maxAge
+  });
+};
 
 // controller actions
 
 //User
 const signup_get = (req, res) => {
-    res.render('signup');
-  };
-  
-const login_get = (req, res) => {
-    res.render('login', { title: 'Login Page' });
-  };
+  res.render('signup');
+};
 
-  const logout_get = (req, res) => {
-    res.cookie('user', '', { maxAge: 1 });
-    res.cookie('teacher', '', { maxAge: 1 });
-    res.redirect('/');
-  };
+const login_get = (req, res) => {
+  res.render('login', { title: 'Login Page' });
+};
+
+const logout_get = (req, res) => {
+  res.cookie('user', '', { maxAge: 1 });
+  res.cookie('teacher', '', { maxAge: 1 });
+  res.redirect('/');
+};
 
 //student
 const student_signup_post = async (req, res) => {
@@ -77,11 +77,11 @@ const student_signup_post = async (req, res) => {
     res.cookie('user', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ student: student._id });
   }
-  catch(err) {
+  catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
   }
- 
+
 };
 
 const student_login_post = async (req, res) => {
@@ -92,7 +92,7 @@ const student_login_post = async (req, res) => {
     const token = createUserToken(student._id);
     res.cookie('user', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(200).json({ student: student._id });
-  } 
+  }
   catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
@@ -102,48 +102,48 @@ const student_login_post = async (req, res) => {
 };
 
 //teacher
-  const teacher_signup_post = async (req, res) => {
-    const { fName, lName, username, password, department } = req.body;
-  
-    try {
-      const teacher = await Teacher.create({ fName, lName, username, password, department  });
-      const tToken = createTeacherToken(teacher._id);
-      res.cookie('teacher', tToken, { httpOnly: true, maxAge: maxAge * 1000 });
-      const token = createUserToken(teacher._id);
-      res.cookie('user', token, { httpOnly: true, maxAge: maxAge * 1000 });
-      res.status(201).json({ teacher: teacher._id });
-    
-    }
-    catch(err) {
-      console.log(err);
-      const errors = handleErrors(err);
-      res.status(400).json({ errors });
-    }
-   
-  };
-  
-  const teacher_login_post = async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      const teacher = await Teacher.login(username, password);
-      const token = createUserToken(teacher._id);
-      res.cookie('user', token, { httpOnly: true, maxAge: maxAge * 1000 });
-      const tToken = createTeacherToken(teacher._id);
-      res.cookie('teacher', tToken, { httpOnly: true, maxAge: maxAge * 1000 });
-      res.status(200).json({ teacher: teacher._id });
-    
-    } 
-    catch (err) {
-      const errors = handleErrors(err);
-      res.status(400).json({ errors });
-    }
-  
-  };
-  
+const teacher_signup_post = async (req, res) => {
+  const { fName, lName, username, password, department } = req.body;
+
+  try {
+    const teacher = await Teacher.create({ fName, lName, username, password, department });
+    const tToken = createTeacherToken(teacher._id);
+    res.cookie('teacher', tToken, { httpOnly: true, maxAge: maxAge * 1000 });
+    const token = createUserToken(teacher._id);
+    res.cookie('user', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ teacher: teacher._id });
+
+  }
+  catch (err) {
+    console.log(err);
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+
+};
+
+const teacher_login_post = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const teacher = await Teacher.login(username, password);
+    const token = createUserToken(teacher._id);
+    res.cookie('user', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    const tToken = createTeacherToken(teacher._id);
+    res.cookie('teacher', tToken, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ teacher: teacher._id });
+
+  }
+  catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+
+};
+
 
 //exports
-module.exports = { 
+module.exports = {
   signup_get,
   login_get,
   logout_get,
